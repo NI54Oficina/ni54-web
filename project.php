965bcb175e9c357b240594876efb665dc922558f
melmiranda <melania.smc@hotmail.com>
2017-01-19 13:17:56 -0300

    13:20 19_01-216

--- project.php
+++ project.php
@@ -42,7 +42,9 @@
 
     <section id="descripcion" class="seccion">
 
-      <div class="col-lg-6 col-md6 col-sm-6 col-xs-12 col-lg-offset-3 col-md-offset-3 center-to-parent">
+      <a id="prev-link" href="">   <div id="prev-port" class="col-lg-3 col-md-3 col-sm-3 col-xs-1 prev-post">   <div  class="prev-label" style="position:fixed; "> <p><span> < </span>ANTERIOR PROYECTO</p> </div> </div></a>
+
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-10 center-to-parent">
 
         <h1>¿De que se trata?</h1>
 
@@ -51,6 +53,10 @@
         <p>At Geckoboard, we're great believers in the Lean methodology. This has lead us to attempt to create a new movement in companies that are truly driven by data. I was keen to coin a 'one word' approach to our manifesto, which is what lead me to pitch 'Informed', a word that can easily be attached to various Business operations to help market our ideas, the Informed Business, informed Sales, etc. </p>
 
       </div>
+
+    <a id="next-link"  href="">  <div id="next-port" class="col-lg-3 col-md-3 col-sm-3 col-xs-1 next-post">  <div class="next-label" style="position:fixed;"><p>PROXIMO PROYECTO <span>></span> </p></div>  </div></a>
+
+
         <!-- <div class="border-white">  </div> -->
     </section>
 
@@ -72,4 +78,44 @@
 
 
   </body>
+
+  <script>
+
+
+
+
+  $( window ).on( "mousemove", function( event ) {
+
+    var mousePosX = event.clientX;
+    var mousePosY = event.clientY;
+    var right = $(window).width() - mousePosX;
+    var bottom = $(window).height() - mousePosY;
+
+
+  //	$(".prev-label").css( "pageX: " + event.pageX + ", pageY: " + event.pageY );
+    $( ".prev-label" ).css( "bottom", + bottom + "px" );
+    $( ".prev-label" ).css( "left", + event.clientX );
+  });
+
+
+
+
+
+
+
+  $( window ).on( "mousemove", function( event ) {
+
+    var mousePosX = event.clientX;
+    var mousePosY = event.clientY;
+    var right = $(window).width() - mousePosX;
+    var bottom = $(window).height() - mousePosY;
+
+  //	$(".prev-label").css( "pageX: " + event.pageX + ", pageY: " + event.pageY );
+    $( ".next-label" ).css( "bottom", + bottom + "px" );
+    $( ".next-label" ).css( "right", + right + "px" );
+  });
+
+
+
+  </script>
 </html>
